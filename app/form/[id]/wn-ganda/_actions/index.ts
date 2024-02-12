fbe53d7e55eeed0d71a794610d80449ce31f297a
vatoer <vatoer.ckplus@gmail.com>
2024-02-12 12:39:31 +0100

    update action wn-ganda'

--- app/form/[id]/wn-ganda/_actions/index.ts
+++ app/form/[id]/wn-ganda/_actions/index.ts
@@ -1,6 +1,8 @@
 "use server";
 import { dbAppointment } from "@/lib/db-appointment";
 import { wnGandaSchema } from "@/lib/zod/wn-ganda";
+import { Prisma } from "@/prisma/db-appointment/generated/client";
+
 import { z } from "zod";
 
 type TFormData = z.infer<typeof wnGandaSchema>;
@@ -21,20 +23,45 @@ export const createFilledFormWnGanda = async (data: TFormData) => {
     };
   }
 
-  const filledForm = await dbAppointment.filledForm.createMany({
-    data: [
-      {
+  try {
+    const filledForm = await dbAppointment.filledForm.create({
+      data: {
         formId: "wn-ganda",
-        createdBy: "John Doe",
         formDataJson: data,
+        createdBy: "tes",
       },
-    ],
-  });
+    });
 
-  return {
-    type: "WNGANDA_CREATE",
-    payload: {
-      data: filledForm,
-    },
-  };
+    return {
+      type: "WNGANDA_CREATE",
+      payload: {
+        data: filledForm,
+      },
+      errors: false,
+    };
+  } catch (error) {
+    //every time we change this code, we must re run pnpm dev
+    // todo create codesanbox untuk ditanyain ke forum
+    let errorCode;
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.log(error);
+      //errorMessage = error.message;
+      errorCode = error.code;
+    }
+
+    if (error instanceof Prisma.PrismaClientUnknownRequestError) {
+      console.log(error);
+      errorCode = "URE001";
+    }
+
+    return {
+      type: "WNGANDA_CREATE",
+      payload: {
+        data: {
+          data,
+        },
+      },
+      errors: errorCode,
+    };
+  }
 };
